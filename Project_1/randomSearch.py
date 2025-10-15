a38e99ab765250a309b5c2dc4a6b2624ab2be887
AkhileshLG <akhileshgenneri@gmail.com>
2025-10-15 09:13:20 -0700

    added calculate distance function and added code for reading .txt files

--- Project_1/randomSearch.py
+++ Project_1/randomSearch.py
@@ -1,13 +1,17 @@
 import sys
+import math
 
 def calculateDistance(xOne, xTwo, yOne, yTwo):
-    return (xTwo - xOne) + (yTwo - yOne)
+    return math.sqrt(math.pow((xTwo - xOne), 2) - math.pow((yTwo - yOne), 2))
 
-def main():
-    fileName = str(input("Enter the name of file: "))
-    yesOrNo = (input("Run Program (y/n): "))
 
-    if yesOrNo == 'y':
-        print()
-    elif yesOrNo == 'n':
-        sys.exit()
+
+fileName = str(input("Enter the name of file: "))
+
+coords = []
+with open(fileName, 'r') as file:
+    for x in file:
+        pairOfCoords = x.strip().split()
+        x = float(pairOfCoords[0])
+        y = float(pairOfCoords[1])
+        coords.append((x,y))
