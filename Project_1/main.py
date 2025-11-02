cc3f927e6c18071f4de6c52797e091f84bc0d783
Harchet Singh <harchetsingh123@gmail.com>
2025-11-02 14:37:02 -0800

    Final Iteration of NN with pruning

--- Project_1/main.py
+++ Project_1/main.py
@@ -1,6 +1,9 @@
 import csv
 from typing import List, Tuple
 import numpy as np
+import matplotlib.pyplot as plt
+import random
+import math
 
 def read_coords_as_tuple(file_path:str) -> List[Tuple[float,float]]: 
     coordinates = []
@@ -32,56 +35,98 @@ def distance_matrix(coordinates):
     return dist_matrix
 
 
-#still need to test/add temperature annealing but basic nearest neighbor is done
-def nn_temperature(matrix):
+def plot_path(coords, path, filename):
+    coords = np.array(coords)
+    plt.figure(figsize=(7,7))
+    plt.scatter(coords[:, 0],coords[:, 1], c='blue')
 
-    path = [0]
-    visited = {0}
+    for i in range(len(path) - 1):
+        x1, y1 = coords[path[i]]
+        x2, y2 = coords[path[i+1]]
+        plt.plot([x1,x2], [y1,y2], 'r-', linewidth=1)
+
+    plt.scatter(coords[path[0], 0], coords[path[0], 1], c = 'green', s = 100, label= 'Start')
+    plt.scatter(coords[path[-1], 0], coords[path[-1], 1], c='red', s=100, label = 'End')
+    plt.title("Best Path So Far")
+    plt.tight_layout()
+    plt.savefig(filename, dpi=300, format ='jpeg')
+    plt.close()
+#still need to test/add temperature annealing but basic nearest neighbor is done
+def nn_temperature(best_path, matrix):
+
+    starting_node = 0
+
+    path = [starting_node]
+    visited = {starting_node}
     path_length = 0
 
-    for i in range(len(matrix) - 1):
+    next_node = random.choice([i for i in range(1,len(matrix))])
+    path.append(next_node)
+    visited.add(next_node)
+    path_length = matrix[starting_node][next_node]
+
+    for i in range(len(matrix) - 2):
         curr_node = path[-1]
         next_node = None
         best_distance = float('inf')
         
         for j in range(len(matrix)):
-            if curr_node in visited:
+            if j in visited:
                 continue
             curr_distance = matrix[curr_node][j]
 
+         
             if curr_distance < best_distance:
-                next_node = curr_node
+                next_node = j
                 best_distance = curr_distance
-                
+    
+        if next_node is None:
+            #not sure if this is correct way to terminate if path length is too large
+            return float('inf'), None
         path_length += best_distance
         path.append(next_node)
         visited.add(next_node)
+        
+        if path_length >= best_path:
+            return float('inf'), None
+
 
     path_length += matrix[path[-1]][0]
     path.append(0)
 
-    return path_length
+    return path_length, path
 
 def find_best_path():
     #need to provide file path
-    coordinate_list = read_coords_as_tuple()
+    file_name = "Dataset_csv/100Triangle300.csv"
+
+    base_name = file_name.rsplit('.', 1)[0]
+    coordinate_list = read_coords_as_tuple(file_name)
 
     dist_matrix = distance_matrix(coordinate_list)
 
-    best_path = float('inf')
+    best_length = float('inf')
+    best_path = None
     #waiting for keyboard interrupt
-    while True:
-        new_path = nn_temperature(dist_matrix)
+    try:
+        while True:
+            path_length, new_path = nn_temperature(best_length, dist_matrix)
 
-        if new_path < best_path:
-            best_path = new_path
-
-    return best_path
-
-
-
+            if path_length < best_length:
+                best_length = path_length
+                best_path = new_path
+                print(f"New best length is {best_length}")
 
 
-    
-    
-    
+    #need to have if statement for keyboard interrupt
+    except KeyboardInterrupt:
+        print("\n Search stopped!")
+        print(f"The best path is: {math.ceil(best_length)}")
+        plot_path(coordinate_list, best_path, f"{base_name}.jpeg")
+        print("\n The path order is: ")
+        print(best_path)
+        return best_path
+
+
+if __name__ == "__main__":
+    find_best_path()
