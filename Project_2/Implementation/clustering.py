beea13d449d8629a40886316e7f93ccef1a5eb31
AkhileshLG <akhileshgenneri@gmail.com>
2025-11-04 23:55:51 -0800

    added file reader and imported  k means from sklearn.cluster

---
+++ Project_2/Implementation/clustering.py
@@ -0,0 +1,22 @@
+from sklearn.cluster import KMeans
+import csv
+
+def read_coords_as_tuple(file_path:str) -> List[Tuple[float,float]]: 
+    coordinates = []
+    with open(file_path, 'r', newline='') as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for row_num, row in enumerate(csv_reader, 1):
+            if len(row) != 2:
+                raise ValueError(f"Row {row_num} must contain 2 values")
+            x, y = map(float, row)
+            coordinates.append((x, y))
+    return coordinates
+
+if __name__ == "__main__":
+    fileName = input("Enter the name of the file: ")
+    tempFileName = "../Dataset/" + fileName
+    coords = read_coords_as_tuple(tempFileName)
+
+    for i in range(1,4):
+        kmeans = KMeans(n_clusters=i)
+        kmeans.fit(coords)
